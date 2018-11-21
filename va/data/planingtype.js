d4d438d45ddd8df56d7a11c2ec2265de1daeb3e1
Austin <zhengbill@users.noreply.github.com>
2018-11-22 02:12:06 +0800

    Code update

---
+++ va/data/planingtype.js
@@ -0,0 +1,205 @@
+var planningtype = {
+  "Bishan": {
+    "overall": 409.0482436,
+    "1-room/2-room": 124.796768,
+    "3-room": 267.7726148,
+    "4-room": 396.7401949,
+    "5-room/executive": 487.2664154
+  },
+  "Bukit Merah": {
+    "overall": 314.3090866,
+    "1-room/2-room": 150.5744527,
+    "3-room": 285.0006353,
+    "4-room": 374.6748467,
+    "5-room/executive": 442.6488769
+  },
+  "Bukit Timah": {
+    "overall": 396.6097504,
+    "1-room/2-room": 0,
+    "3-room": 275.8785308,
+    "4-room": 377.192616,
+    "5-room/executive": 469.6045591
+  },
+  "Geylang": {
+    "overall": 338.7797342,
+    "1-room/2-room": 163.737405,
+    "3-room": 280.2067502,
+    "4-room": 402.1817724,
+    "5-room/executive": 492.8105413
+  },
+  "Kallang": {
+    "overall": 310.4165678,
+    "1-room/2-room": 136.3625437,
+    "3-room": 288.8140337,
+    "4-room": 387.7180474,
+    "5-room/executive": 457.5783733
+  },
+  "Marine Parade": {
+    "overall": 315.9179818,
+    "1-room/2-room": 186.54356,
+    "3-room": 276.7772156,
+    "4-room": 355.8908981,
+    "5-room/executive": 431.6453031
+  },
+  "Novena": {
+    "overall": 346.292135,
+    "1-room/2-room": 174.4000912,
+    "3-room": 298.0336536,
+    "4-room": 371.4336922,
+    "5-room/executive": 483.4150246
+  },
+  "Outram": {
+    "overall": 251.3604066,
+    "1-room/2-room": 136.4982677,
+    "3-room": 288.9003294,
+    "4-room": 368.6389671,
+    "5-room/executive": 492.806817
+  },
+  "Queenstown": {
+    "overall": 297.6729904,
+    "1-room/2-room": 162.9037863,
+    "3-room": 263.6983805,
+    "4-room": 351.9678325,
+    "5-room/executive": 448.9547519
+  },
+  "Rochor": {
+    "overall": 327.3895728,
+    "1-room/2-room": 179.975229,
+    "3-room": 293.0006427,
+    "4-room": 378.3512498,
+    "5-room/executive": 487.6876666
+  },
+  "Tanglin": {
+    "overall": 425.9010932,
+    "1-room/2-room": 0,
+    "3-room": 330.4432896,
+    "4-room": 404.3763644,
+    "5-room/executive": 466.9205531
+  },
+  "Toa Payoh": {
+    "overall": 326.0755861,
+    "1-room/2-room": 155.8419356,
+    "3-room": 287.7877624,
+    "4-room": 372.9899103,
+    "5-room/executive": 465.3901275
+  },
+  "Bedok": {
+    "overall": 349.6320509,
+    "1-room/2-room": 153.1838149,
+    "3-room": 276.2596794,
+    "4-room": 385.4711163,
+    "5-room/executive": 478.16247
+  },
+  "Pasir Ris": {
+    "overall": 479.6620752,
+    "1-room/2-room": 210.1162987,
+    "3-room": 62.56253721,
+    "4-room": 419.4198297,
+    "5-room/executive": 523.5470182
+  },
+  "Tampines": {
+    "overall": 422.847803,
+    "1-room/2-room": 158.6789322,
+    "3-room": 293.9677603,
+    "4-room": 417.6394501,
+    "5-room/executive": 512.8606547
+  },
+  "Ang Mo Kio": {
+    "overall": 316.4137845,
+    "1-room/2-room": 160.1191325,
+    "3-room": 277.5930849,
+    "4-room": 377.9788662,
+    "5-room/executive": 447.918193
+  },
+  "Hougang": {
+    "overall": 400.0077296,
+    "1-room/2-room": 158.5275454,
+    "3-room": 277.2316705,
+    "4-room": 399.7423441,
+    "5-room/executive": 493.5566778
+  },
+  "Punggol": {
+    "overall": 380.7235997,
+    "1-room/2-room": 96.34985811,
+    "3-room": 125.8808036,
+    "4-room": 342.8025787,
+    "5-room/executive": 425.8555259
+  },
+  "Sengkang": {
+    "overall": 396.2805603,
+    "1-room/2-room": 129.9249856,
+    "3-room": 187.9027218,
+    "4-room": 362.2731679,
+    "5-room/executive": 438.3165227
+  },
+  "Serangoon": {
+    "overall": 399.7036689,
+    "1-room/2-room": 161.9455768,
+    "3-room": 268.8802518,
+    "4-room": 397.6064536,
+    "5-room/executive": 507.2120581
+  },
+  "Sembawang": {
+    "overall": 399.2566274,
+    "1-room/2-room": 100.1238054,
+    "3-room": 71.94165896,
+    "4-room": 358.8793648,
+    "5-room/executive": 438.8100301
+  },
+  "Woodlands": {
+    "overall": 402.9724622,
+    "1-room/2-room": 141.1617527,
+    "3-room": 265.7141142,
+    "4-room": 378.8016126,
+    "5-room/executive": 476.0958415
+  },
+  "Yishun": {
+    "overall": 376.0770025,
+    "1-room/2-room": 164.7395322,
+    "3-room": 273.0282553,
+    "4-room": 389.9079638,
+    "5-room/executive": 490.9567833
+  },
+  "Bukit Batok": {
+    "overall": 362.7593164,
+    "1-room/2-room": 145.5336461,
+    "3-room": 264.259842,
+    "4-room": 379.9216361,
+    "5-room/executive": 470.2077685
+  },
+  "Bukit Panjang": {
+    "overall": 379.5586163,
+    "1-room/2-room": 82.20749283,
+    "3-room": 260.4973756,
+    "4-room": 359.5483059,
+    "5-room/executive": 432.8523138
+  },
+  "Choa Chu Kang": {
+    "overall": 406.0866568,
+    "1-room/2-room": 158.7906309,
+    "3-room": 271.5748174,
+    "4-room": 376.2251073,
+    "5-room/executive": 458.2984938
+  },
+  "Clementi": {
+    "overall": 318.1781811,
+    "1-room/2-room": 139.3317575,
+    "3-room": 265.5189223,
+    "4-room": 363.762293,
+    "5-room/executive": 452.8971796
+  },
+  "Jurong East": {
+    "overall": 366.8035748,
+    "1-room/2-room": 148.1631375,
+    "3-room": 271.2906317,
+    "4-room": 376.1344783,
+    "5-room/executive": 450.7012516
+  },
+  "Jurong West": {
+    "overall": 393.2542806,
+    "1-room/2-room": 179.0412577,
+    "3-room": 271.2021763,
+    "4-room": 387.2380858,
+    "5-room/executive": 462.8920343
+  }
+};
